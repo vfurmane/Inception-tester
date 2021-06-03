b04c62a4ee9d87cf63a970f1a8288910247ac9fa
vfurmane <vfurmane@student.42.fr>
2021-06-03 12:34:44 +0200

    Add tests to check that NGINX is 301 redirected to HTTPS

--- index.test.js
+++ index.test.js
@@ -1,14 +1,45 @@
+const https = require('https');
 const axios = require('axios');
 require('dotenv').config({
 	path: '.env.testing'
 });
 
-describe(`when contacting http://${process.env.NGINX_CONTAINER}:80`, function() {
+describe(`when contacting http://${process.env.NGINX_CONTAINER}`, function() {
+
+	const agent = new https.Agent({
+		rejectUnauthorized: false
+	});
 
 	test('should resolve', async function() {
-		const response = axios.get(`http://${process.env.NGINX_CONTAINER}:80`);
+		const response = axios({
+			url: `http://${process.env.NGINX_CONTAINER}`,
+			httpsAgent: agent,
+		});
 
 		await expect(response).resolves.toEqual(expect.anything());
+	});
+
+	test('should throw 301 error', async function() {
+		const response = axios({
+			url: `http://${process.env.NGINX_CONTAINER}`,
+			httpsAgent: agent,
+			maxRedirects: 0
+		});
+		
+		await expect(response).rejects.toThrowError('301');
+	});
+
+	test('should redirect to https', async function() {
+		const response = axios({
+			url: `http://${process.env.NGINX_CONTAINER}`,
+			httpsAgent: agent,
+		});
+		
+		await expect(response).resolves.toMatchObject({
+			request: {
+				protocol: 'https:'
+			}
+		});
 	});
 
 });
